270197bcd465ff8257b6e90e93e730a0ae712fe8
Pham Doan Hieu <hieubagiang@yahoo.com.vn>
2023-10-08 21:13:54 +0700

    Update setup_doskey.ps1

--- setup_doskey.ps1
+++ setup_doskey.ps1
@@ -1,11 +1,13 @@
 # Đường dẫn đến tệp preload_doskey.bat trong UserProfile
 $doskeyScriptPath = "$env:USERPROFILE\preload_doskey.bat"
+Write-Host "Tool preload alias by Pham Doan Hieu"
 
 # URL của tệp preload_doskey.bat trên GitHub
 $doskeyScriptURL = "https://raw.githubusercontent.com/hieubagiang/public-script/main/preload_doskey.bat"
-
+Write-Host "Fetch alias from remote..."
 # Fetch preload_doskey.bat from Remote
 Invoke-WebRequest -Uri $doskeyScriptURL -OutFile $doskeyScriptPath
+Write-Host "Thêm alias vào AutoRun của Command Prompt..."
 
 # Thêm tệp preload_doskey.bat vào AutoRun của Command Prompt
 $regPath = "HKCU:\Software\Microsoft\Command Processor"
@@ -18,4 +20,4 @@ if (-not (Test-Path -Path $regPath)) {
 
 Set-ItemProperty -Path $regPath -Name $regName -Value $regValue
 
-Write-Host "Đã tải và cấu hình tệp preload_doskey.bat thành công."
+Write-Host "Cấu hình alias thành công."
