5d65a1249bd0b79f74f4129ead90dbfd70934910
Pham Doan Hieu <hieubagiang@yahoo.com.vn>
2025-08-17 16:39:52 +0700

    Update preload_doskey.bat

--- preload_doskey.bat
+++ preload_doskey.bat
@@ -1,12 +1,22 @@
 @echo off
+:: Git aliases
 doskey gpf=git push -f $*
 doskey gp=git push $*
-doskey gc=git commit -m $1
+doskey gpo=git push origin $*
+doskey gc=git commit -m $*
 doskey gs=git status
 doskey gf=git fetch
 doskey ga=git add .
-doskey fb=fvm flutter packages pub run build_runner build --delete-conflicting-outputs
+doskey gca=git commit --amend $*
+doskey grc=git rebase --continue $*
+doskey grba=git rebase -i --autosquash $*
+
+:: Flutter / Dart / Makefile aliases
+doskey fb=fvm dart run build_runner build --delete-conflicting-outputs
+doskey fbw=fvm dart run build_runner watch --delete-conflicting-outputs
 doskey f=fvm flutter $*
-doskey fpa=fvm flutter pub add $*
-doskey fpg=fvm flutter pub get
-doskey fclr=fvm flutter clean
+doskey fpg=f pub get
+doskey fc=f clean
+doskey fpa=f pub add $*
+doskey fg=make gen
+doskey ml=make lang
